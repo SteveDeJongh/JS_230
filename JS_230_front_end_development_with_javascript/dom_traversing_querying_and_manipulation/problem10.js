fea4129e4a84f52c14e861dbbfd5e673d8e30263
Steve De Jongh <de.jongh.steve@gmail.com>
2024-01-16 13:24:26 -0800

    JS230 lesson 1

---
+++ JS_230_front_end_development_with_javascript/dom_traversing_querying_and_manipulation/problem10.js
@@ -0,0 +1,30 @@
+// 10 HTML Imaging
+
+// Start Html:
+
+// <!doctype html>
+// <html>
+//   <head>
+//     <title>HTML Imaging</title>
+//   </head>
+//   <body>
+//   </body>
+// </html>
+
+// Resulting HTML
+<!doctype html>
+<html>
+  <head>
+    <title>HTML Imaging</title>
+    <style type="test/css">
+      .emphasis { font-weight: bold; }
+      .light { color: gray; }
+    </style>
+  </head>
+  <body>
+    <header id="header">
+      <h1 class="emphasis light">Dynamic Content</h1>
+      <p>Hello World!</p>
+    </header>
+  </body>
+</html>
