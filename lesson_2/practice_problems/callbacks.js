7766f11f662f24d7a863d2b54f5b47d2ba144be7
Steve De Jongh <de.jongh.steve@gmail.com>
2024-01-18 09:27:35 -0800

    JS 230 Lesson 2

--- lesson_2/practice_problems/callbacks.js
+++ lesson_2/practice_problems/callbacks.js
@@ -21,12 +21,42 @@
 
 // 3
 
-function processData(nums, callback) {
-  setTimeout(() => {
-    return nums.maps(callback);
-  }, 1000)
+// function processData(nums, callback) {
+//   setTimeout(() => {
+//     let result = nums.map(callback);
+//     console.log(result);
+//   }, 1000)
+// }
+
+// console.log(processData([1,2,3], (number) => number * 2));
+
+// 4
+
+// function waterfallOverCallbacks(callbacks, num) {
+//   let result = num;
+//   callbacks.forEach(callback => result = callback(result));
+//   return result;
+// }
+
+// const double = (x) => x * 2;
+
+// console.log(waterfallOverCallbacks([double, double, double], 1));
+
+// 5
+
+function startCounter(callback) {
+  let counter = 0;
+  let interval = setInterval(() => {
+    counter += 1;
+    if (callback(counter)) {
+      clearInterval(interval);
+    }
+  }, 1000);
 }
 
-console.log(processData([1,2,3], (number) => number * 2));
+const log = function(x) {
+  console.log(x);
+  return x === 5;
+}
 
-// ^ Todo...
+startCounter(log);
