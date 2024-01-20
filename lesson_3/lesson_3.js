e40542729b9421a25daa9f0db638eb5df005b8e8
Steve De Jongh <de.jongh.steve@gmail.com>
2024-01-20 15:25:33 -0800

    JS230 Lesson 3 XMLHttpRequests

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -4,3 +4,18 @@
 
 // Network Programming in JavaScript
 
+// Making a Request with XMLHttpRequest
+
+// Practice Problems:
+
+// 1
+
+let request2 = new XMLHttpRequest();
+request2.open('GET', 'https://api,github.com/repos/rails/rails');
+request2.send();
+
+// 2
+
+request2.responseText;
+
+// XMLHttpRequest Events
