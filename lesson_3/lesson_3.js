6b2d0c1092cd9d5950bd574697d9cada29c070c6
Steve De Jongh <de.jongh.steve@gmail.com>
2024-01-21 13:29:54 -0800

    JS230 Lesson 3

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -19,3 +19,32 @@ request2.send();
 request2.responseText;
 
 // XMLHttpRequest Events
+
+// Data Serialization
+
+// Example: Loading HTML via XHR
+
+// Practice problems:
+
+// 1
+
+// We receive a ERR file not found error when trying to submit as we submit a post request to `;//products/15
+
+// Example: Submitting a Form via XHR
+
+// 1
+
+store.addEventListener('submit', event => {
+  event.preventDefault();
+
+  let form = event.target;
+  let request = new XMLHttpRequest();
+  let data = new FormData(form);
+
+  request.open(form.method, `https://ls-230-web-store-demo.herokuapp.com/${form.getAttribute('action')}`);
+
+  request.addEventListener('load', event => store.innerHTML = request.response);
+  request.send();
+});
+
+// 2
