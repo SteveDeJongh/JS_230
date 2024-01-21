f263ffefac15d9085692c42272185bdcb010ab3c
Steve De Jongh <de.jongh.steve@gmail.com>
2024-01-21 15:04:23 -0800

    JS230 Lesson 3

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -44,7 +44,93 @@ store.addEventListener('submit', event => {
   request.open(form.method, `https://ls-230-web-store-demo.herokuapp.com/${form.getAttribute('action')}`);
 
   request.addEventListener('load', event => store.innerHTML = request.response);
-  request.send();
+  request.send(data);
 });
 
 // 2
+
+request.setRequestHeader('Authorization', 'token AUTH_TOKEN');
+
+// Example: Loading JSON via XHR
+
+// 1
+
+let request = new XMLHttpRequest();
+request.open('GET', 'https://api.github.com/repos/rails/rails');
+request.responseType = 'json';
+
+request.addEventListener('load', event => {
+  console.log(request.status);
+  console.log(request.response.open_issues);
+})
+
+request.send();
+
+// Alternatively
+
+let request = new XMLHttpRequest();
+request.open('GET', 'https://api.github.com/repos/rails/rails');
+
+request.addEventListener('load', event => {
+  let data = JSON.parse(request.response);
+  console.log(request.status);
+  console.log(data.open_issues);
+});
+
+request.send();
+
+// 2
+
+let request = new XMLHttpRequest();
+request.open('GET', 'hts://api.github.com/repos/rails/rails');
+
+request.addEventListener('load', event => {
+  let data = JSON.parse(request.response);
+  console.log(request.status);
+  console.log(data.open_issues);
+});
+
+request.addEventListener('error', event => {
+  console.log('The request could not be completed!');
+});
+
+request.send();
+
+// Example: Sending JSON via XHR
+
+// 1
+
+// POST /books HTTP/1.1
+// Host: https://lsjs230-book-catalog.herokuapp.com
+// Content-Length: 
+// Content-Type: application/json; charset=utf-8
+// Accept: */*
+
+// { 'title': 'Eloquent JavaScript', 'author': 'Marijn Haverbeke' }
+
+// 2
+
+function createProduct(newObj) {
+  let request = new XMLHttpRequest();
+  request.open('POST', 'https://ls-230-web-store-demo.herokuapp.com/v1/products');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.setRequestHeader('Authorization', 'token AUTH_TOKEN');
+  let newObjJson = JSON.stringify(newObj);
+
+  request.addEventListener('load', () => {
+    console.log(`This product was added: ${request.responseText}`);
+  });
+
+  request.send(newObjJson);
+}
+
+let newObj = {
+  name: 'newobj',
+  sku: 'new123',
+  price: 100,
+}
+
+createProduct(newObj);
+
+// Cross-Domain XMLHttpRequests with CORS
+
