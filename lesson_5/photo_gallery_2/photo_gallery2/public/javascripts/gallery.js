d77b24d31f374f3de14a1ef0b6ee63994dc904a7
Steve De Jongh <de.jongh.steve@gmail.com>
2024-01-29 15:25:13 -0800

    JS230 Lesson 5

--- lesson_5/photo_gallery_2/photo_gallery2/public/javascripts/gallery.js
+++ lesson_5/photo_gallery_2/photo_gallery2/public/javascripts/gallery.js
@@ -30,6 +30,7 @@ document.addEventListener('DOMContentLoaded', e => {
       return item.id === idx;
     })[0];
     let header = document.querySelector('section > header');
+    header.innerHTML = "";
     header.insertAdjacentHTML('beforeend', templates.photo_information(photo));
   }
 
@@ -38,9 +39,63 @@ document.addEventListener('DOMContentLoaded', e => {
       .then(response => response.json())
       .then(comment_json => {
         let comment_list = document.querySelector('#comments > ul');
+        comment_list.innerHTML = "";
         comment_list.insertAdjacentHTML('beforeend', templates.photo_comments({comments: comment_json}));
     });
   };
+
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = new FormData(form);
+    let currentSlideID =  slideshow.currentSlide.getAttribute('data-id');
+    data.set('photo_id', currentSlideID);
+    let queryString = new URLSearchParams([...data]);
+    // let queryString = [];
+    // for (const [k, v] of data) {
+    //   queryString.push(k + '=' + v);
+    // };
+    // queryString = queryString.join('&');
+    fetch('/comments/new', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      body: queryString,
+    })
+    .then(response => response.json())
+    .then(json => {
+      let commentsList = document.querySelector('#comments > ul');
+      commentsList.insertAdjacentHTML('beforeend', templates.photo_comment(json));
+      form.reset();
+    })
+  });
+
+  document.querySelector('section > header').addEventListener('click', function(e) {
+    e.preventDefault();
+    let button = e.target;
+    let buttonType = button.getAttribute("data-property");
+    if (buttonType) {
+      let href = button.getAttribute('href');
+      let dataId = button.getAttribute('data-id');
+      let text = button.textContent;
+
+      fetch(href, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        body: 'photo_id=' + dataId,
+      })
+      .then(response => response.json())
+      .then(json => {
+        button.textContent = text.replace(/\d+/, json.total);
+        fetch("/photos")
+          .then(response => response.json())
+          .then(json => photos = json);
+      });
+    }
+  });
 
   const slideshow = {
     prevslide: function(e) {
@@ -88,7 +143,7 @@ document.addEventListener('DOMContentLoaded', e => {
       prevButton.addEventListener('click', (e) => { this.prevslide(e) });
       nextButton.addEventListener('click', (e) => { this.nextSlide(e) });
     },
-    
+
     init: function() {
       this.slideshow = document.querySelector('#slideshow');
       let slides = this.slideshow.querySelectorAll('figure');
