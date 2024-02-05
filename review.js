015cc1830f971e0867663d0b332293d4d54fcb9a
Steve De Jongh <de.jongh.steve@gmail.com>
2024-02-05 12:04:54 -0800

    JS239 prep

---
+++ review.js
@@ -0,0 +1,19 @@
+// JS 239 Assessment Prep
+
+// Lesson 1 review
+
+// Lesson 2 review 
+
+// function delayLog() {
+//   for (let i = 1; i <= 10; i += 1) {
+//     let logger = function() { console.log(i)};
+//     setTimeout(logger, i * 1000);
+//   }
+// }
+
+// delayLog();
+
+// function afterNSeconds(cb, time) {
+//   setTimeout(cb, time * 1000);
+// }
+
