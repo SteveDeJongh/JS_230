d820ac9f549328787f094ad3a873f153cce9d0ce
Steve De Jongh <de.jongh.steve@gmail.com>
2024-02-05 15:23:49 -0800

    JS239 prep

--- review.js
+++ review.js
@@ -101,3 +101,94 @@
 // }
 
 // processData([1, 2, 3], (number) => number * 2);
+
+// function waterfallOverCallbacks(cbs, start) {
+//   let res = start;
+//   cbs.forEach(cb => res = cb(res));
+//   console.log(res);
+// }
+
+// // Example usage:
+// const double = (x) => x * 2;
+// waterfallOverCallbacks([double, double, double], 1);
+// // Logs: 8
+
+// function startCounter(callback) {
+//   let counter = 0;
+//   const intervalId = setInterval(() => {
+//     counter++;
+//     if (callback(counter)) {
+//       clearInterval(intervalId);
+//     }
+//   }, 1000);
+// }
+
+// // Example usage:
+// startCounter((count) => {
+//   console.log(count);
+//   return count === 5;
+// });
+// // Logs 1, 2, 3, 4, 5, then stops
+
+// function downloadFilePromise() {
+//   return new Promise((resolve, reject) => {
+//     console.log('Download starting...');
+//     setTimeout(() => {
+//       resolve('Download complete!');
+//     }, 1500);
+//   });
+// }
+
+// downloadFilePromise()
+//   .then((r) => console.log(r))
+//   .catch((e) => console.log('Error! ' + e));
+
+// function processDataPromise(arr) {
+//   return new Promise((resolve, reject) => {
+//     console.log('Working...');
+//     setTimeout(() => {
+//       let processed = arr.map(x => x * 2);
+//       resolve(processed);
+//     } , 1000);
+//   })
+// }
+
+// processDataPromise([1,2,3])
+//   .then((r) => console.log('Success, ' + r))
+//   .catch((e) => console.log('Ruh roh + e'));
+
+// function flakyService() {
+//   return new Promise((resolve, reject) => {
+//     let num = Math.floor(Math.random() * 10);
+
+//     if ( num >= 5) {
+//       resolve('Operation success!')
+//     } else {
+//       reject('Operation failed.')
+//     }
+//   });
+// }
+
+// flakyService()
+//   .then((s) => console.log('Success! ' + s))
+//   .catch((e) => console.log('Fail ' + e));
+
+// function cleanupFiles() {
+//   return new Promise((resolve, reject) => {
+//     console.log('Starting operation.');
+//     if (true) {
+//       resolve('Operation successfull.');
+//     }
+//   })
+// }
+
+// cleanupFiles()
+//   .then((s) => console.log(s))
+//   .finally(() => { console.log('Cleaning up resources...')});
+
+// new Promise((resolve, reject) => {
+//   resolve(7);
+// }).then((r) => {return r * 2})
+//   .then((r) => r + 5)
+//   .then(console.log);
+
