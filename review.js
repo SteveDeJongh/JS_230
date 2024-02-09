95ec4d3edf0231db01db4f756f59a6bab7c1fbfd
Steve De Jongh <de.jongh.steve@gmail.com>
2024-02-08 16:02:31 -0800

    JS239 prep

--- review.js
+++ review.js
@@ -430,23 +430,23 @@
 
 // request.send();
 
-function createProduct(productData) {
-  let json = JSON.stringify(productData);
-  let request = new XMLHttpRequest();
+// function createProduct(productData) {
+//   let json = JSON.stringify(productData);
+//   let request = new XMLHttpRequest();
 
-  request.open('POST', 'https://ls-230-web-store-demo.herokuapp.com/v1/products');
-  request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-  request.setRequestHeader('Authorization', 'token AUTH_TOKEN');
+//   request.open('POST', 'https://ls-230-web-store-demo.herokuapp.com/v1/products');
+//   request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+//   request.setRequestHeader('Authorization', 'token AUTH_TOKEN');
 
-  request.addEventListener('load', () => {
-    console.log(`This product was added: ${request.responseText}`);
-  });
+//   request.addEventListener('load', () => {
+//     console.log(`This product was added: ${request.responseText}`);
+//   });
 
-  request.send(json);
-}
+//   request.send(json);
+// }
 
-createProduct({
-  name: 'HB pencil',
-  sku: 'hbp100',
-  price: 50,
-});
+// createProduct({
+//   name: 'HB pencil',
+//   sku: 'hbp100',
+//   price: 50,
+// });
