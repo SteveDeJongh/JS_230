a2a09fba8939238b4d455de1c5e2040b9eff62f0
Steve De Jongh <de.jongh.steve@gmail.com>
2024-02-19 11:44:03 -0800

    JS239 Practice Project v2

--- CMNV2/public/javascripts/app.js
+++ CMNV2/public/javascripts/app.js
@@ -1,30 +1,56 @@
-console.log('hello');
+class App {
+  constructor() {
+    this.main = document.querySelector('main');
 
-async function fetchContacts() {
-  const response = await fetch('http://localhost:3000/api/contacts');
-  return await response.json();
+    // Temp tags handler
+    this.tags = ['school', 'work', 'friend'];
+
+    // Home Page
+    this.homeTemplate = Handlebars.compile(document.querySelector('#home-temp').innerHTML);
+
+    // Add-Conctact Form
+    this.addContactTemplate = Handlebars.compile(document.querySelector('#add-contact-form-temp').innerHTML);
+  }
+ 
+  // Home Page
+  async fetchContactsData() {
+    const response = await fetch('http://localhost:3000/api/contacts');
+    return await response.json();
+  }
+  
+  tagsStringToArray(contacts) {
+    return contacts.map(contact => {
+      if (contact.tags) {
+        contact.tags = contact.tags.split(',');
+      }
+      return contact;
+    })
+  }
+  
+  async renderHomePage() {
+    let contacts = await this.tagsStringToArray(await this.fetchContactsData());
+    let html = this.homeTemplate({contacts: contacts});
+    this.main.innerHTML = html;
+  }
+
+  bindHomePageEvents() {
+    let addContact = document.querySelector('.add-contact');
+
+    addContact.addEventListener('click', this.renderAddContactForm.bind(this));
+  }
+
+  // Add contact form
+  renderAddContactForm() {
+    let html = this.addContactTemplate({tags: this.tags});
+    this.main.innerHTML = html;
+  }
+
+
+
+  async init() {
+    await this.renderHomePage();
+    this.bindHomePageEvents();
+  }
 }
 
-let mainTempScript = document.querySelector('#main-temp').innerHTML;
-let mainTemplate = Handlebars.compile(mainTempScript);
-
-let contacts;
-
-function tagsToArray(contacts) {
-  return contacts.map(contact => {
-    if (contact.tags) {
-      contact.tags = contact.tags.split(',');
-    }
-    return contact;
-  })
-}
-
-async function asignContacts() {
-  let dataObj = await fetchContacts()
-  console.log(dataObj);
-  dataObj = tagsToArray(dataObj);
-  console.log(dataObj);
-  let html = mainTemplate({contacts: dataObj});
-  console.log(html);
-}
-asignContacts();
+let app = new App().init();
