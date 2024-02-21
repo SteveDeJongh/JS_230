45e9ec6a36d2b119273413d8ad20e4f84924995d
Steve De Jongh <de.jongh.steve@gmail.com>
2024-02-20 16:47:43 -0800

    JS 239 Practice Project v2

--- CMNV2/public/javascripts/app.js
+++ CMNV2/public/javascripts/app.js
@@ -1,24 +1,18 @@
+const DEFAULT_TAGS = ['school', 'work', 'friend'];
+
 class App {
   constructor() {
-    this.main = document.querySelector('main');
-
-    // Temp tags handler
-    this.tags = ['school', 'work', 'friend'];
-
-    // Home Page
+    // Templates
     this.homeTemplate = Handlebars.compile(document.querySelector('#home-temp').innerHTML);
     this.contactLITemplate = Handlebars.compile(document.querySelector('#contact-li-template').innerHTML);
-    this.contacts;
-
-    // Add-Conctact Form
     this.addContactTemplate = Handlebars.compile(document.querySelector('#add-contact-form-temp').innerHTML);
-    this.form;
-
-    // Edit-Contact Form
     this.editContactTemplate = Handlebars.compile(document.querySelector('#edit-contact-form-temp').innerHTML);
-
-    // Tags
     this.tagTemplate = Handlebars.compile(document.querySelector('#selected-tag-template').innerHTML);
+        
+    this.main = document.querySelector('main');
+    this.contacts;
+    this.form;
+    this.tags;
 
     this.init();
   }
@@ -39,8 +33,7 @@ class App {
   }
   
   async renderHomePage() {
-    this.contacts = await this.tagsStringToArray(await this.fetchContactsData());
-    let html = this.homeTemplate({contacts: this.contacts});
+    let html = this.homeTemplate({tags: this.tags, contacts: this.contacts});
     this.main.innerHTML = html;
   }
 
@@ -58,9 +51,57 @@ class App {
     });
 
     searchBar.addEventListener('keyup', this.searchBarInput.bind(this));
+    let tagFilters = document.querySelectorAll('.main-page-tags input');
+    tagFilters.forEach(button => button.addEventListener('click', this.tagSelection.bind(this)));
+
+    this.main.addEventListener('click', this.tagListener.bind(this));
   }
 
-  // Edit Actions
+  tagSelection(e) {
+    let tagFilters = [...document.querySelectorAll('.main-page-tags input')];
+    let contactList = document.querySelector('.contacts-list');
+    let tags = tagFilters.filter(tag => tag.checked);
+    tags = tags.map(tag => tag.value)
+    let filterResults = this.filterByTagSelection(tags);
+    contactList.innerHTML = this.contactLITemplate({contact: filterResults});
+  }
+
+  filterByTagSelection(tags) {
+    return this.contacts.filter(contact => {
+      contact.tags = contact.tags || [];
+      return tags.every(tag => contact.tags.includes(tag));
+    });
+  }
+
+  async init() {
+    this.contacts = await this.tagsStringToArray(await this.fetchContactsData());
+    this.tags = this.populateTags.call(this, this.contacts);
+    await this.renderHomePage();
+    this.bindHomePageEvents();
+
+    return this;
+  }
+
+  // Search Function
+  searchBarInput(e) {
+    let contactList = document.querySelector('.contacts-list');
+    let match = e.target.value;
+    let filterResults = this.filterContacts(match);
+    contactList.innerHTML = this.contactLITemplate({contact: filterResults});
+  }
+
+  filterContacts(query) {
+    query = query.toLowerCase();
+    return this.contacts.filter(contact => {
+      contact.tags = contact.tags || [];
+      return contact.full_name.toLowerCase().includes(query) ||
+             contact.email.toLowerCase().includes(query) ||
+             contact.phone_number.includes(query) ||
+             contact.tags.some(tag => tag.toLowerCase().includes(query))
+    })
+  }
+
+  // Edit Contact Actions
   async handleEditClick(e) {
     let id = Number(e.target.dataset.contactId);
     let contact = await this.fetchContactDataByID(id);
@@ -94,6 +135,13 @@ class App {
       input.addEventListener('blur', this.handleInputBlur.bind(this));
     });
     newTagBtn.addEventListener('click', this.addTagHandler.bind(this));
+    
+    let tagInput = document.querySelector('.add-tag-input');
+    tagInput.addEventListener('keydown', (e) => {
+      if (e.key !== 'Enter') return;
+      e.preventDefault();
+      newTagBtn.dispatchEvent(new Event('click'));
+    });
   }
 
   editContactFormCancel(e) {
@@ -107,15 +155,7 @@ class App {
     let id = Number(editForm.dataset.contactId);
     if (editForm.checkValidity()) {
       let formData = new FormData(editForm);
-      let dataObj = {tags: []};
-      for (const [k,v] of formData) {
-        if (k === 'tags') {
-          dataObj[k].push(v);
-        } else {
-          dataObj[k] = v;
-        }
-      }
-      dataObj.tags = dataObj.tags.join(',');
+      let dataObj = this.formatFormData(formData);
       dataObj.id = id;
   
       let response = await this.submitEditContactData(dataObj);
@@ -146,7 +186,7 @@ class App {
     }
   }
 
-  // Delete Actions
+  // Delete Contact Actions
 
   async handleDeleteClick(e) {
     let id = Number(e.target.dataset.contactId);
@@ -172,25 +212,6 @@ class App {
     }
   }
 
-  // Search
-  searchBarInput(e) {
-    let contactList = document.querySelector('.contacts-list');
-    let match = e.target.value;
-    let filterResults = this.filterContacts(match);
-    contactList.innerHTML = this.contactLITemplate({contact: filterResults});
-  }
-
-  filterContacts(query) {
-    query = query.toLowerCase();
-    return this.contacts.filter(contact => {
-      contact.tags = contact.tags || [];
-      return contact.full_name.toLowerCase().includes(query) ||
-             contact.email.toLowerCase().includes(query) ||
-             contact.phone_number.includes(query) ||
-             contact.tags.some(tag => tag.toLowerCase().includes(query))
-    })
-  }
-
   // Add contact form
   renderAddContactForm() {
     let html = this.addContactTemplate({tags: this.tags});
@@ -212,29 +233,43 @@ class App {
       input.addEventListener('blur', this.handleInputBlur.bind(this));
     });
     newTagBtn.addEventListener('click', this.addTagHandler.bind(this));
+
+    let tagInput = document.querySelector('.add-tag-input');
+    tagInput.addEventListener('keydown', (e) => {
+      if (e.key !== 'Enter') return;
+      e.preventDefault();
+      newTagBtn.dispatchEvent(new Event('click'));
+    });
+  }
+
+  formatFormData(formData) {
+    let dataObj = {tags: []};
+    for (const [k,v] of formData) {
+      if (k === 'tags') {
+        dataObj[k].push(v);
+      } else {
+        dataObj[k] = v;
+      }
+    }
+    dataObj.tags = dataObj.tags.join(',');
+    return dataObj;
   }
 
   async addContactFormSubmit(e) {
     e.preventDefault(); 
     if (this.form.checkValidity()) {
       let formData = new FormData(this.form);
-      let dataObj = {tags: []};
-      for (const [k,v] of formData) {
-        if (k === 'tags') {
-          dataObj[k].push(v);
-        } else {
-          dataObj[k] = v;
-        }
-      }
-      dataObj.tags = dataObj.tags.join(',');
-  
+      let dataObj = this.formatFormData(formData);
       let response = await this.submitNewContactData(dataObj);
+
       if (response.status === 201) {
         this.init();
       } else {
         alert('Failed to submit new contact data.');
       }
     } else {
+      let inputs = this.form.querySelectorAll('input');
+      inputs.forEach(input => this.validateInput(input));
       alert('Please correct form errors before submitting.')
     }
   }
@@ -265,7 +300,7 @@ class App {
     let field = e.target;
     let eSpan = document.querySelector("span[data-name='" + 'error-' + field.name + "']");
     eSpan.textContent = '';
-    eSpan.classList.remove('invalid');
+    field.classList.remove('invalid');
   }
 
   handleInputBlur(e) {
@@ -288,39 +323,32 @@ class App {
     let eSpan = document.querySelector("span[data-name='" + 'error-' + field.name + "']");
     let eMsg = eSpan.dataset.fieldName + ' is a required field.';
     eSpan.textContent = eMsg;
-    eSpan.classList.add('invalid');
+    field.classList.add('invalid');
   }
 
   handleFormValueMismatch(field) {
     let eSpan = document.querySelector("span[data-name='" + 'error-' + field.name + "']");
-    let eMsg = "Please entere a valid " + eSpan.dataset.fieldName + '.';
+    let eMsg = "Please enter a valid " + eSpan.dataset.fieldName + '.';
     eSpan.textContent = eMsg;
-    eSpan.classList.add('invalid');
+    field.classList.add('invalid');
   }
 
-  // End of Add contact
-
-  async init() {
-    await this.renderHomePage();
-    this.tags = this.populateTags.call(this, this.contacts);
-    this.bindHomePageEvents();
-
-    return this;
-  }
-
-  // tags
+  // Tags
 
   populateTags(contacts) {
     let allTags = contacts.flatMap(contact => contact.tags ? contact.tags : []);
     let tags = allTags.filter((tag, idx) => allTags.indexOf(tag) === idx);
-    return tags.length > 0 ? tags : ['work', 'friend', 'school']; // default tag options present if no contacts exist.
+    return tags.length > 0 ? tags : DEFAULT_TAGS; // default tag options present if no contacts exist.
   }
 
   addTagHandler(e) {
     let newTagVal = e.target.previousElementSibling.value.toLowerCase();
+    if (newTagVal.length === 0) return;
     if (this.tags.includes(newTagVal)) {
+      e.target.previousElementSibling.value = '';
       alert('That tag already exists.');
     } else {
+      e.target.previousElementSibling.value = '';
       this.tags.push(newTagVal);
       this.addTagAndSelect(newTagVal);
     }
@@ -330,6 +358,19 @@ class App {
     let tagField = document.querySelector('.tag-options');
     let html = this.tagTemplate({tag: newTag});
     tagField.insertAdjacentHTML('beforeend', html);
+  }
+
+  tagListener(e) {
+    let target = e.target;
+    if (!target.classList.contains('contact-tag')) return;
+
+    let tagFilterButton = this.main.querySelector(`.main-page-tags input[value="${target.textContent}"]`);
+    if (tagFilterButton.checked) {
+      tagFilterButton.checked = false;
+    } else {
+      tagFilterButton.checked = true;
+    }
+    this.tagSelection();
   }
 }
 
